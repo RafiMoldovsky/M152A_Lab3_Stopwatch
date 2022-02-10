93a873a97b3283338675c18cb81a9c9b8cdd85d5
Jacob Zhang <jacob.b.zhang@gmail.com>
2022-02-10 13:42:45 -0800

    Update debouncer.v

--- debouncer.v
+++ debouncer.v
@@ -19,38 +19,51 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module debouncer(
-    input clk,
+    input clk_sys,
     input button,
     output button_output
 );
-reg [9:0] count=10'b1000000000; 
+reg [4:0] count=5'b00000; 
 reg button_output_reg;
 wire [3:0] count_sum;
 
+reg [17:0] clk_divider;
+
+always @(posedge clk_sys) clk_divider = clk_divider+1;
+
+assign clk = clk_divider[17];
+
+reg [1:0] button_input_ff;
+
+/*
+always @(posedge clk or posedge button) begin
+if (button)
+button_input_ff <= 2'b11;
+else
+button_input_ff <= {1'b0,button_input_ff[1]};
+end
+	
+*/
 
 always @(posedge clk) begin
     if(button==0)
-    begin
-        count<={count[8:0],0};
-        if(count_sum<=3) begin
-            button_output_reg<=0;
-        end
-    end
+        count<=(count << 1);
     else
     begin
-        count<={count[8:0],1'b1};
-        if(count_sum>=7)
-        begin
-            count <= 0;
-            button_output_reg<=1;
-        end
-        else if(count_sum<=3) begin
-            button_output_reg<=0;
-        end
+        count<=(count<<1)|1;
     end
 end
 
-    assign count_sum= count[9]+count[8]+count[7]+count[6]+count[5]+count[4]+count[3]+count[2]+count[1]+count[0];
+always @(posedge clk) begin
+        if(count_sum<=1) begin
+            button_output_reg<=0;
+        end else if (count_sum>=3)
+        begin
+            button_output_reg<=1;
+        end
+end
+
+    assign count_sum= count[4]+count[3]+count[2]+count[1]+count[0];
     assign button_output=button_output_reg;
 
 endmodule 
